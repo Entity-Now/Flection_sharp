609ca58335f5033b61fa2f4bc3fea96eadef800f
Entity-now <2966388213@qq.com>
2024-06-13 11:47:09 +0800

    更新项目结构

---
+++ test/Program.cs
@@ -0,0 +1,16 @@
+﻿using Flection_Sharp;
+using test.test;
+
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = flecion.getSingleType("test", "MyTestClass");
+            var data = flecion.dynamicToObject(type, new { Title = "the is title", value = "the is value" }) as MyTestClass;
+            Console.WriteLine(data.title);
+        }
+    }
+}
